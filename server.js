093d9d6519f5a8483f1eae16cd44f5fe3d11d91a
HanzsX <130972657+HanzsX@users.noreply.github.com>
2026-01-14 22:17:36 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+
+// IZINKAN HTML DARI MANA SAJA
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = "API_KEY_KAMU";
+const PROJECT = "raff-coffe";
+
+app.post("/qris", async (req, res) => {
+  try {
+    const { order_id, amount } = req.body;
+
+    const response = await fetch(
+      "https://app.pakasir.com/api/transactioncreate/qris",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          project: PROJECT,
+          order_id,
+          amount,
+          api_key: API_KEY
+        })
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("API running"));
